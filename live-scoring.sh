a19eee7486a02c6a43a86d31984f93269ee40bda
Ryan Dwyer <ryanpdwyer@gmail.com>
2020-12-05 09:02:35 -0500

    Updated the auto-loading stuff

--- live-scoring.sh
+++ live-scoring.sh
@@ -2,11 +2,15 @@
 
 function updateScoring {
 
+    echo `date +"%A %I:%M %p"` > update_time.txt
     python sleeper_sel.py
     git commit -am "Auto-update: `date`"
     git push
 
 }
+
+
+gsed -i 's/auto_update = False/auto_update = True/g' first_app.py
 
 while :
 do 
@@ -14,3 +18,5 @@ do
     updateScoring
     sleep 300 # Wait 5 minutes
 done
+
+gsed -i 's/auto_update = True/auto_update = False/g' first_app.py
